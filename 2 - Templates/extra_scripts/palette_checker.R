ffa3381baf457ed592fce3a1d87ef13f6eadc251
Andy Baker <Andrew.Baker1@dhsc.gov.uk>
2022-11-23 20:09:36 +0000

    geometry convertion examples, shape_convert with weights and points, improved palette checker

--- 2 - Templates/extra_scripts/palette_checker.R
+++ 2 - Templates/extra_scripts/palette_checker.R
@@ -1,51 +1,81 @@
-# Colour palette accessibility checker (work in progress)
+# Colour palette accessibility checker
 
 options(pkgType = "binary")
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(
+
+p_load(
   tidyverse,
   janitor,
-  savonliquide
+  savonliquide,
+  gt,
+  scales
 )
 
 
 check_palette <- function(p) {
-  # choose the palette to check here
+  
   palette <- tolower(p)
+  
   palette_wbg <- c(
     "#ffffff", # white
     "#000000", # black
     "#999999", # grey60, commonly used for missing data
-    "#CCCCCC", # grey80, commonly used for missing data
+    "#cccccc", # grey80, commonly used for missing data
     palette
   )
   
-  test <- sapply(palette_wbg, function(a)
+  ratios <- sapply(palette_wbg, function(a)
     sapply(palette_wbg, function(b)
       check_contrast_raw(a,b)$ratio)) %>% 
     data.frame() %>% 
     clean_names() %>% 
     rownames_to_column("compare_to")
   
-  test %>% 
+  table <- ratios %>% 
     pivot_longer(cols = -compare_to) %>% 
     mutate(
       value = as.numeric(value),
-      name = factor(
-        str_replace(name, "x_", "#"),
-        levels = c(palette_wbg),
-        ordered = TRUE
+      name = str_replace(name, "x_", "#"),
+      new_name = factor(case_when(
+        name == "#ffffff" ~ "White",
+        name == "#000000" ~ "Black",
+        name == "#999999" ~ "grey60",
+        name == "#cccccc" ~ "grey80",
+        TRUE ~ name
+      ),
+      levels = c("White", "Black", "grey60", "grey80", palette),
+      ordered = TRUE
+      ),
+      Colours = factor(case_when(
+        compare_to == "#ffffff" ~ "White",
+        compare_to == "#000000" ~ "Black",
+        compare_to == "#999999" ~ "grey60",
+        compare_to == "#cccccc" ~ "grey80",
+        TRUE ~ compare_to
+      ),
+      levels = c("White", "Black", "grey60", "grey80", palette),
+      ordered = TRUE
       )
     ) %>% 
-    filter(value >= 3) %>% 
-    pivot_wider(names_from = name, values_from = value)
+    select(-name, -compare_to) %>% 
+    pivot_wider(
+      names_from = new_name, 
+      values_from = value
+    )
+  
+  table %>% 
+    gt() %>% 
+    data_color(
+      columns = names(table)[-1],
+      colors = col_numeric(
+        palette = c("#D7EFC3", "#88D147"),
+        domain = c(3, 21),
+        na.color = "#000000"
+      )
+    ) %>% 
+    suppressWarnings()
   
 }
 
-DHSC_accessible_5 <- c("#512698", "#E57200", "#1A9CCB", "#2F4D14", "#D06DCE")
-DHSC_accessible_5_g <- c("#294011", "#4C721D", "#589325", "#88D147", "#D7EFC3")
-viridis_quintile <- c("#440154", "#3b528b", "#21918c", "#5ec962", "#fde725")
-ibm_palette <- c("#648FFF", "#785EF0", "#DC267F", "#FE6100", "#FFB000")
-
-check_palette(DHSC_accessible_5_g)
-
+# DHSC_accessible_5_g <- c("#294011", "#4C721D", "#589325", "#88D147", "#D7EFC3")
+# check_palette(DHSC_accessible_5)
--- 2 - Templates/extra_scripts/shape_convert.R
+++ 2 - Templates/extra_scripts/shape_convert.R
@@ -1,9 +1,24 @@
-# Script for crudely converting data from one shapefile to another, even if 
-# areas don't perfectly overlap.
+# shape_convert() takes a measure with shape or point geography (old_shape) and 
+# a new shape to convert the measure to (new_shape).
 
-# Currently, this function only gives the weighted average from overlapping 
-# areas. So this wouldn't work for summing up count data from a group of 
-# smaller areas up to one biggest area, for example.
+# This function creates a new measure column calculated as a weighted average 
+# from overlapping areas when type = "mean", and sums up the weighted count when 
+# type = "sum".
+
+# weight = "none", take the unweighted sum/mean of all points or areas that 
+# overlap.
+
+# weight = "weighting", weight the sum/mean of all points or areas that overlap 
+# by a given measure.
+
+# weight = "overlap", take the sum/mean of all areas that overlap, weighted by
+# the % overlap of old and new areas.
+
+# weight = "weighting and overlap", take the sum/mean of all areas that overlap, 
+# weighted by the % overlap of old and new areas plus a given measure.
+
+# Your weighting column should be included in old_shape, if required.
+
 
 options(pkgType = "binary")
 if (!require("pacman")) install.packages("pacman")
@@ -13,36 +28,58 @@ pacman::p_load(
   dplyr
 )
 
-shape_convert <- function(old_shape, new_shape, type = "mean") {
+shape_convert <- function(old_shape, new_shape, weight, type) {
   
-  intersect_pct <- st_intersection(old_shape, new_shape) %>% 
-    mutate(intersect_area = st_area(.)) %>%
+  # Calculate the area of overlap between old and new areas
+  intersect_pct <<- st_intersection(old_shape, new_shape) %>% 
+    mutate(
+      intersect_area = st_area(.),
+      weighting = ifelse(str_detect(weight, "weighting"), weighting, 1)
+      ) %>%
     select(
       area_code,
       area_code.1,
-      intersect_area
+      intersect_area,
+      weighting
     ) %>%
-    st_drop_geometry()
+    st_drop_geometry() %>% 
+    mutate(intersect_area = ifelse(weight == "none", 1, intersect_area))
   
-  old_shape_joined <- old_shape %>% 
-    mutate(county_area = st_area(old_shape)) %>% 
+  # Join the overlap data back to old shape to compare with original area
+  old_shape_joined <<- old_shape %>% 
+    mutate(county_area = ifelse(weight == "none", 1, st_area(old_shape))) %>% 
+    select(-contains("weighting")) %>% # so we don't end up with two of the same column
     merge(intersect_pct, by = "area_code", all.x = TRUE)
   
+  # Calculate % area overlap, then aggregate measure based on this
   converted_measure <- old_shape_joined %>% 
-    mutate(prop_inter = as.numeric(intersect_area/county_area)) %>% 
+    mutate(
+      prop_inter = case_when(
+        weight == "none" ~ 1, 
+        weight == "weighting" ~ weighting, 
+        weight == "overlap" ~ as.numeric(intersect_area/county_area), 
+        weight == "weighting and overlap" ~ weighting + as.numeric(intersect_area/county_area) 
+      )
+    ) %>% 
     st_drop_geometry() %>% 
     select(
       area_code,
       area_code.1,
       prop_inter,
+      weighting,
       measure
+    ) %>% 
+    group_by(area_code) %>% 
+    mutate(
+      prop_of_prop_inter_old = prop_inter/sum(prop_inter, na.rm = TRUE),
+      check_100_old = sum(prop_of_prop_inter_old),
     ) %>% 
     group_by(area_code.1) %>% 
     mutate(
-      prop_of_prop_inter = prop_inter/sum(prop_inter, na.rm = TRUE),
-      check_100 = sum(prop_of_prop_inter),
+      prop_of_prop_inter_new = prop_inter/sum(prop_inter, na.rm = TRUE),
+      check_100_new = sum(prop_of_prop_inter_new),
       measure_weighted = case_when(
-        type == "mean" ~ measure*prop_of_prop_inter,
+        type == "mean" ~ measure*prop_of_prop_inter_new,
         type == "sum" ~ measure*prop_inter
       ),
       measure_new = sum(measure_weighted, na.rm = TRUE)
@@ -51,9 +88,10 @@ shape_convert <- function(old_shape, new_shape, type = "mean") {
     select(
       area_code.1,
       measure_new
-    ) %>% 
+    ) %>%
     distinct()
   
+  # Join back to shapefile geometry
   new_shape_measure <- left_join(
     new_shape, 
     converted_measure, 
